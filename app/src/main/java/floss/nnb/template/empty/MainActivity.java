0cf1030890797757d49a5dc7222b8eaea4acaf22
nonaybay <rafaelvenancio@protonmail.com>
2022-03-06 15:34:29 -0300

    empty app, template to use on fdroid

---
+++ app/src/main/java/floss/nnb/template/empty/MainActivity.java
@@ -0,0 +1,13 @@
+package floss.nnb.template.empty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
